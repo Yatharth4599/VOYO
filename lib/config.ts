1b4384f720d8680da7e45902069f63d8faba5bd8
rithvikrthampy <rithvikrthampy@gmail.com>
2025-06-21 06:31:48 +0400

    url change

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 // API Configuration
 export const API_CONFIG = {
-  //BASE_URL: 'http://3.83.195.172:3000',
-  BASE_URL: 'http://0.0.0.0:3000',
+  BASE_URL: 'http://3.83.195.172:3000',
+  //BASE_URL: 'http://0.0.0.0:3000',
 } as const;
 
 // Helper function to create API URLs
